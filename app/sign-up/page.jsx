a455f820fb6bdceaa9cfa1d6f6fd4e3e0046b29b
Yogesh-shimpi <yogeshshimpi320@gmail.com>
2025-01-09 23:07:44 +0530

    form handling in sign up page of all in one desginer

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,71 +1,146 @@
-"use client"
-import React, { useState } from "react";
+"use client";
+import React, { useState, useRef } from "react";
 import Image from "next/image";
 
 import "@/app/sign-up/sign_up.css";
-import visibility from '@/app/assets/visibility.svg'
-import visibility_off from '@/app/assets/visibility_off.svg'
+import visibility from "@/app/assets/visibility.svg";
+import visibility_off from "@/app/assets/visibility_off.svg";
+import { useForm } from "react-hook-form";
 
 const page = () => {
-  const [inputBorderPassword,setinputBorderPassword] = useState(false)
-  const [inputBorderConfirm,setinputBorderConfirm] = useState(false)
-  const [EyePassword,setEyePassword] = useState(false)
-  const [EyeConfirm,setEyeConfirm] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm();
+  const [EyePassword, setEyePassword] = useState(false);
+  const [EyeConfirm, setEyeConfirm] = useState(false);
+  const password = watch("password");
+ 
+  const handleInputType = (e) => {
+    if (e == 0) {
+      setEyePassword(!EyePassword);
+    } else {
+      setEyeConfirm(!EyeConfirm);
+    }
+  };
 
-  const handleBorderFocus = (e) =>{
-    if(e == 0 ){
-      setinputBorderPassword(true)
-    }else{
-      setinputBorderConfirm(true)
-    }
-  }
-  const handleBorderBlur = (e) =>{
-    if(e == 0 ){
-      setinputBorderPassword(false)
-    }else{
-      setinputBorderConfirm(false)
-    }
-  }
-  const handleInputType = (e) =>{
-    if(e == 0 ){
-      setEyePassword(!EyePassword)
-    }else{
-      setEyeConfirm(!EyeConfirm)
-    }
-  }
+  const onSubmit = (date) => {
+    
+  };
+
   return (
     <main>
       <section className="hero">
         <div className="heading">Sign Up</div>
         <div className="sub-heading">Create New Account</div>
-        <form action="">
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
           <div className="username">
-            <input type="text" placeholder="username" />
-            <div className="error">error</div>
+            <input
+              type="text"
+              placeholder="username"
+              {...register("username", {
+                required: { value: true, message: "Username is required" },
+                minLength: { value: 3, message: "Minimum length 3 required" },
+                maxLength: { value: 15, message: "Maximum length 15 required" },
+                pattern: {
+                  value: /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d).+$/,
+                  message:
+                    "Username should contain special character and numeric value",
+                },
+              })}
+            />
+            <div className="error">
+              {errors.username && errors.username.message}
+            </div>
           </div>
           <div className="email">
-            <input type="text" placeholder="Email" />
-            <div className="error">error</div>
+            <input
+              type="text"
+              placeholder="Email"
+              {...register("email", {
+                required: { value: true, message: "Email is required" },
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.(com)$/i,
+                  message: "Please enter valid email",
+                },
+              })}
+            />
+            <div className="error">{errors.email && errors.email.message}</div>
           </div>
           <div className="password">
-            <div className="password-field" style={{borderColor:inputBorderPassword?"#7c7c7c":"transparent"}} >
-              <input type={EyePassword?"password":"text"} onFocus={()=>{handleBorderFocus(0)}} onBlur={()=>{handleBorderBlur(0)}} placeholder="Password"/>
-              <button className="pass" type="button" onClick={()=>{handleInputType(0)}}><Image src={EyePassword?visibility_off:visibility}  className="image" alt=""/></button>
+            <div className="password-field">
+              <input
+                type={EyePassword ? "password" : "text"}
+                placeholder="Password"
+                {...register("password", {
+                  required: { value: true, message: "Password is required" },
+                  minLength: { value: 3, message: "Minimum length 3 required" },
+                  maxLength: {
+                    value: 15,
+                    message: "Maximum length 15 required",
+                  },
+                  pattern: {
+                    value: /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d).+$/,
+                    message:
+                      "Password should contain special character and numeric value",
+                  },
+                })}
+              />
+              <button
+                className="pass"
+                type="button"
+                onClick={() => {
+                  handleInputType(0);
+                }}>
+                <Image
+                  src={EyePassword ? visibility_off : visibility}
+                  className="image"
+                  alt=""
+                />
+              </button>
             </div>
-            <div className="error">error</div>
+            <div className="error">
+              {errors.password && errors.password.message}
+            </div>
           </div>
           <div className="confirm-password">
-            <div className="password-field" style={{borderColor:inputBorderConfirm?"#7c7c7c":"transparent"}}>
-              <input type={EyeConfirm?"password":"text"} onFocus={()=>{handleBorderFocus(1)}} onBlur={()=>{handleBorderBlur(1)}} placeholder="Confirm password"/>
-              <button className="pass" type="button" onClick={()=>{handleInputType(1)}}><Image src={EyeConfirm?visibility_off:visibility} className="image" alt=""/></button>
+            <div className="password-field">
+              <input
+                type={EyeConfirm ? "password" : "text"}
+                placeholder="Confirm password"
+                {...register("confirmPassword", {
+                  validate: (value) =>
+                    value === password || "Passwords do not match",
+                })}
+              />
+              <button
+                className="pass"
+                type="button"
+                onClick={() => {
+                  handleInputType(1);
+                }}>
+                <Image
+                  src={EyeConfirm ? visibility_off : visibility}
+                  className="image"
+                  alt=""
+                />
+              </button>
             </div>
-            <div className="error">error</div>
+            <div className="error">
+              {errors.confirmPassword && errors.confirmPassword.message}
+            </div>
           </div>
-          <button className="submit" type="submit">Create account</button>
+          <button className="submit" type="submit">
+            Create account
+          </button>
         </form>
         <span className="or"></span>
         <div className="sign_in">
-          <div>Already hava an account? <span>Sign In</span></div>
+          <div>
+            Already hava an account? <span>Sign In</span>
+          </div>
         </div>
       </section>
     </main>
